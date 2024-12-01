d4b86327c790e144f497a51644ff6bdfd4ca4f2f
Jayreddin <jayreddin@hotmail.com>
2024-12-01 00:10:27 +0000

    jj
    jj

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jamie/Documents/Ai_Assist/venv"
+set -gx VIRTUAL_ENV "/home/jamie/Documents/GitHub/Ai_AgentJamie/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
